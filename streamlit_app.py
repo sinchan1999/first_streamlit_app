784bc6a0824a29c47d5d6aeacc5c90b7e532caa9
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-21 15:12:25 +0530

    Update streamlit_app.py
    Adding up few food items for Breakfast, Lunch and Dinner!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,16 @@
 import streamlit
 streamlit.title("Hey Sinchan this side! Starting on Streamlit with Food Menu.")
+
+streamlit.header("Breakfast items")
+streamlit.text("Bread & Jam/Butter")
+streamlit.text("Watermelon, Banana, Maskmelon, Grapes")
+streamlit.text("Tea/Coffee")
+
+streamlit.header("Lunch items")
+streamlit.text("Chicken Biriany/Paneer Biriany")
+streamlit.text("Fish curry / Mushroom curry")
+streamlit.text("Ice-cream")
+
+streamlit.header("Dinner items")
+streamlit.text("Rice/Roti")
+streamlit.text("Chicken curry")
