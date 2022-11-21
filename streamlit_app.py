02a592bd22a4f55ccadef20b4f12f54a2bd45929
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-21 15:19:34 +0530

    Update streamlit_app.py
    Breakfast menu changed  and lots of Emojis are inserted!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit
 streamlit.title("Hey Sinchan this side! Starting on Streamlit with Food Menu.")
 
-streamlit.header("Breakfast items")
-streamlit.text("Bread & Jam/Butter")
-streamlit.text("Watermelon, Banana, Maskmelon, Grapes")
-streamlit.text("Tea/Coffee")
+streamlit.header('Breakfast Menu')
+streamlit.text(🍛'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞'Avocado Toast')
+
 
 streamlit.header("Lunch items")
-streamlit.text("Chicken Biriany/Paneer Biriany")
-streamlit.text("Fish curry / Mushroom curry")
-streamlit.text("Ice-cream")
+streamlit.text(🍗"Chicken Biriany/Paneer Biriany")
+streamlit.text(🐟"Fish curry / Mushroom curry")
+streamlit.text(🍨"Ice-cream")
 
-streamlit.header("Dinner items")
+streamlit.header("Dinner items") 
 streamlit.text("Rice/Roti")
 streamlit.text("Chicken curry")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
