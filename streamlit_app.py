c0cbf5e50c5cd215545b02837c55cfdb482d2d0b
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-21 15:21:18 +0530

    Update streamlit_app.py
    Emojis must be inside " "

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,16 @@ import streamlit
 streamlit.title("Hey Sinchan this side! Starting on Streamlit with Food Menu.")
 
 streamlit.header('Breakfast Menu')
-streamlit.text(🍛'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text("🍛Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞Avocado Toast")
 
 
 streamlit.header("Lunch items")
-streamlit.text(🍗"Chicken Biriany/Paneer Biriany")
-streamlit.text(🐟"Fish curry / Mushroom curry")
-streamlit.text(🍨"Ice-cream")
+streamlit.text("🍗Chicken Biriany/Paneer Biriany")
+streamlit.text("🐟Fish curry / Mushroom curry")
+streamlit.text("🍨Ice-cream")
 
 streamlit.header("Dinner items") 
 streamlit.text("Rice/Roti")
