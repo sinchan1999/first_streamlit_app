4fda824c5ddb159b2707fd52336cc97c6ca92215
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-22 19:50:03 +0530

    Update streamlit_app.py
    Adding New section to display Fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# New section to display Fruityvice API
+streamlit.header("Fruityvice Fruit Advice!")
 
 #Let's Call the Fruityvice API from Our Streamlit App
 import requests
