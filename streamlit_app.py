b3a03c7a75fe6745eca7f75440da45e31aa49f80
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-24 16:02:12 +0530

    Update streamlit_app.py
    changing button title.

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ def get_fruit_load_list():
         return  my_cur.fetchall()
       
 #Adding button to load the fruit
-if streamlit.button('Get fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
