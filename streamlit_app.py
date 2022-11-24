7a192e1d9c52cc95a1f8adc4eff818f1da01c486
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-24 14:33:33 +0530

    Update streamlit_app.py
    Function added for the repeated code block!

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,22 +37,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# New section to display Fruityvice API
-streamlit.header("Fruityvice Fruit Advice!")
 
+#Creating a funtion for the repeatable code block
+def get_fruityvice_data(this_fruit_choice):
+   #Let's Call the Fruityvice API from Our Streamlit App
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    #For Displaying the response  -->  #streamlit.text(fruityvice_response)  
+    
+    # Normalizing the JSON version of the response 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalize
+  
+ # New section to display Fruityvice API
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   #Adding a Text Entry Box for sending the input
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information..!!!")
   else:
-    #Let's Call the Fruityvice API from Our Streamlit App
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #For Displaying the response
-    #streamlit.text(fruityvice_response)
-    
-    # Normalizing the JSON version of the response 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+   
     
     # Output as a table
     streamlit.dataframe(fruityvice_normalized)
