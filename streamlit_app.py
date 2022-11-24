8356d31ac7bb5541195b3a5c6945e28e768952a5
SINCHAN PANDA <86179752+sinchan1999@users.noreply.github.com>
2022-11-24 12:21:15 +0530

    Update streamlit_app.py
    Querying All the Rows in fruit_load_list from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,10 +54,10 @@ streamlit.dataframe(fruityvice_normalized)
 #Setting Up Streamlit to Work with Snowflake
 import snowflake.connector
 
-#Trying to Query Our Trial Account Metadata 
+#Trying to Query All the Rows in fruit_load_list from snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains :")
+streamlit.dataframet(my_data_rows)
